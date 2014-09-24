64cbb014e09a8277b60176a59e556859d54aa669
Anton Kirillov <akirillov.mailbox@gmail.com>
2014-09-24 22:52:27 +0400

    [+] <about> handler and stub added

---
+++ app/controllers/UtilController.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import securesocial.core.RuntimeEnvironment
+import auth.{SecureSocialAuth, SocialUser}
+import play.api.libs.json.Json
+import play.api.Logger
+import dao.TagDao
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class UtilController (override implicit val env: RuntimeEnvironment[SocialUser])
+  extends securesocial.core.SecureSocial[SocialUser]  with SecureSocialAuth{
+
+  private val logger = Logger("[UtilController]")
+
+  def about = UserAwareAction {
+    implicit request =>
+      Ok(views.html.about(request.user))
+  }
+}
